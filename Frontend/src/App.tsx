ff6a880ea1a9ea365c61443b9f392175b36935f2
Saksham Mahnot <sakshammahnot@Sakshams-MacBook-Air.local>
2026-03-02 19:31:39 +0530

    Fix things

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Header from './Component/Header/Header'
-import Footer from './Component/Footer/Footer'
+import Footer from './Component/Footer/footer'
 import Home from './Component/Home/Home'
 import EmployeeDashboard from './Component/Dashboard/EmployeeDashboard'
 import { Toaster } from 'react-hot-toast'
